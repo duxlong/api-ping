d780351180f60452a1bcfb8013157815463f6306
duxlong <duxlong@me.com>
2020-11-06 16:22:20 +0800

    Create api-ping.py

---
+++ api-ping.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def ping():
+    host = request.args.get('host')
+
+    # ping host
+    res = os.system('ping -c 1 -W 1 %s' % host)
+
+    return 'off' if bool(res) else 'on'
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
